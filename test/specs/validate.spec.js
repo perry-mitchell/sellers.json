a68ae27e3e42b76e5fc6c5828241748a28756fb4
Perry Mitchell <perry@kiosked.com>
2019-06-11 14:32:43 +0300

    Add tests for assert method

--- test/specs/validate.spec.js
+++ test/specs/validate.spec.js
@@ -4,6 +4,20 @@ const SELLERS_JSON_INVALID = require("../resources/sellers.invalid.json");
 const SELLERS_JSON_VALID = require("../resources/sellers.valid.json");
 
 describe("validate", function() {
+    describe("assertValidSellersJSON", function() {
+        it("throws no error for valid files", function() {
+            expect(() => {
+                assertValidSellersJSON(SELLERS_JSON_VALID);
+            }).to.not.throw();
+        });
+
+        it("throws for invalid files", function() {
+            expect(() => {
+                assertValidSellersJSON(SELLERS_JSON_INVALID);
+            }).to.throw(/sellers\.5\.name cannot be empty/);
+        });
+    });
+
     describe("validateSellersJSON", function() {
         it("validates with no errors for correct files", function() {
             const errors = validateSellersJSON(SELLERS_JSON_VALID);
